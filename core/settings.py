b11af80b26654137b7aa81b95922ec01a85112be
miles <miles.pickull@web.de>
2025-10-22 04:48:04 +0200

    feat(cors): add cors headers bypass

--- core/settings.py
+++ core/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = 'django-insecure-7^+%vn298$b%7+_k(py905@&l%!csx=@z+fb0-w)==x)-a6km@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 # Application definition
 
